5d6e756c4df9b3460611dc6c86b6f5ab7439951d
Richard Samuelson <noobtoothfairy@gmail.com>
2016-01-22 21:40:28 +0100

    Big change
    Dynamic template

---
+++ js/main.js
@@ -0,0 +1,12 @@
+angular.module('app', ['ui.router'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider.state('home', {
+		url: '/home',
+		templateUrl: 'home.html'
+	}).state('about', {
+
+	});
+});
