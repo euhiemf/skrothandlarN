a0c91893278cbc701bb54fd26cb666435bba1da9
Richard Samuelson <noobtoothfairy@gmail.com>
2016-01-22 23:24:30 +0100

    It is clean

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 angular.module('app', ['ui.router'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/home');
+	//$urlRouterProvider.otherwise('/home');
 
 	$stateProvider.state('home', {
 		url: '/home',
 		templateUrl: 'home.html'
+	}).state('shop', {
+		url: '/shop',
+		templateUrl: 'shop.html'
+	}).state('item', {
+		url: '/item/:itemIndex',
+		templateUrl: 'item.html'
 	}).state('about', {
-
-	});
+		url: '/about',
+		templateUrl: 'about.html'
+	})
+	;
 });
